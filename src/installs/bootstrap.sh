f36f846e2f41b32db72061e4accbaf2563565d2f
mjprobinson <mjprobinson15@gmail.com>
2025-08-18 21:12:58 -0400

    Move dotfiles install to bootstrap so I know they are there before neovim and tmux install.

--- src/installs/bootstrap.sh
+++ src/installs/bootstrap.sh
@@ -2,3 +2,8 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git jq stow
+
+git clone https://github.com/mjprobinson/.dotfiles.git $HOME/
+pushd $HOME/.dotfiles/
+stow .
+popd
--- src/installs/dotfiles.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-git clone https://github.com/mjprobinson/.dotfiles.git ~/
-pushd ~/.dotfiles/
-stow .
-popd
