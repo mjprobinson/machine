1e41ea317a4917cabee2be9ce1cdddb3e23a6e30
mjprobinson <mjprobinson15@gmail.com>
2025-08-18 20:56:23 -0400

    Fix install location

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/bash
 
 root_dir=$(dirname $(realpath "$0"))
+install_location="$HOME/.local/share/applications"
 
 chmod +x "$root_dir/bin/machine"
-if [ -d $HOME/.local/applications/machine ]; then
-    cp -rf "$root_dir" $HOME/.local/applications
+if [ -d "$install_location/machine" ]; then
+    cp -rf "$root_dir" "$install_location"
 else
-    cp -r "$root_dir" $HOME/.local/applications
+    cp -r "$root_dir" "$install_location"
 fi
-pushd $HOME/.local/applications/machine
-ln -sf $HOME/.local/applications/machine/bin/machine $HOME/.local/bin
+pushd "$install_location/machine"
+ln -sf "$install_location/machine/bin/machine" $HOME/.local/bin
 popd
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 
 rm $HOME/.local/bin/machine
-rm -rf $HOME/.local/share/machine
+rm -rf $HOME/.local/share/applications/machine
 rm -rf $HOME/.local/applications/machine
