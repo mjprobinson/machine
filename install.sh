053ab45ffd3f19a5f7351f0bfb3e06bba37b297e
mjprobinson <mjprobinson15@gmail.com>
2025-08-21 06:35:05 -0400

    Change install script to mv instead of cp source dir

--- install.sh
+++ install.sh
@@ -5,11 +5,9 @@ install_location="$HOME/.local/share/applications"
 
 chmod +x "$root_dir/bin/machine"
 if [ -d "$install_location/machine" ]; then
-    # TODO, change this to mv instead of cp when testing is done
-    cp -rf "$root_dir" "$install_location"
+    mv -rf "$root_dir" "$install_location"
 else
-    # TODO, change this to mv instead of cp when testing is done
-    cp -r "$root_dir" "$install_location"
+    mv -r "$root_dir" "$install_location"
 fi
 pushd "$install_location/machine"
 ln -sf "$install_location/machine/bin/machine" $HOME/.local/bin/
