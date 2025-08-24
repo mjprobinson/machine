7dea2e7da5a7f28a7ea866d68d4192352cfad1ad
mjprobinson <mjprobinson15@gmail.com>
2025-08-23 21:24:50 -0400

    Add mounting smb share to fstab

---
+++ src/installs/smb-share.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+bw_unlock
+
+# https://itslinuxfoss.com/mount-smb-shares-ubuntu-22-04/
+config=$(bw list items --search "michael-smb-creds" | jq -r '.[].notes')
+fstab_line="//truenas.local/smb_share /mnt/smb_share cifs uid=0,credentials=$HOME/.smbcredentials,iocharset=utf8,vers=3.0,noperm 0 0"
+echo "$config" > $HOME/.smbcredentials
+chmod 400 $HOME/.smbcredentials
+
+sudo bash <<EOF
+  apt install -y cifs-utils
+  mkdir /mnt/smb_share
+  echo "$fstab_line" >> /etc/fstab
+  mount -a
+EOF
