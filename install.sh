8e7b2e8bdab4fb39c260ec3cf52c8c508f1301fb
mjprobinson <mjprobinson15@gmail.com>
2025-08-24 18:48:07 -0400

    Didn't work

--- install.sh
+++ install.sh
@@ -1,23 +1,20 @@
 #!/usr/bin/bash
 
-main()  {
-    root_dir=$(dirname $(realpath "$0"))
-    install_location="$HOME/.local/share/applications"
+root_dir=$(dirname $(realpath "$0"))
+install_location="$HOME/.local/share/applications"
 
-    chmod +x "$root_dir/bin/machine"
-    if [ -d "$install_location/machine" ]; then
-        mv -f "$root_dir" "$install_location"
-    else
-        mv "$root_dir" "$install_location"
-    fi
+chmod +x "$root_dir/bin/machine"
+if [ -d "$install_location/machine" ]; then
+    mv -f "$root_dir" "$install_location"
+else
+    mv "$root_dir" "$install_location"
+fi
 
-    if [ ! -d $HOME/.local/bin/ ]; then
-        mkdir $HOME/.local/bin
-    fi
-    pushd "$install_location/machine"
-    ln -sf "$install_location/machine/bin/machine" $HOME/.local/bin/
-    popd
-}
+if [ ! -d $HOME/.local/bin/ ]; then
+    mkdir $HOME/.local/bin
+fi
+pushd "$install_location/machine"
+ln -sf "$install_location/machine/bin/machine" $HOME/.local/bin/
+popd
 
 export PATH=$PATH:$HOME/.local/bin/
-bash main
