a4e4df203a203f952623f80c09744b778228b823
mjprobinson <mjprobinson15@gmail.com>
2025-09-08 20:11:49 -0400

    video group in sway install

--- src/installs/sway.sh
+++ src/installs/sway.sh
@@ -2,3 +2,7 @@ sudo apt install -y sway swayidle swaylock waybar grimshot blueman nm-tray brigh
 
 # For screen sharing
 sudo apt install -y xdg-desktop-portal xdg-desktop-portal-wlr
+
+# Add user to video group to skip sudo for brightnessctl
+sudo usermod -aG video $USER
+newgrp video
