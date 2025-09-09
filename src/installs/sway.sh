afd2b48cd277de73019ded3d91dfccd3e8cb2f8a
mjprobinson <mjprobinson15@gmail.com>
2025-09-08 20:09:04 -0400

    Missing packages for audio and video control in sway

--- src/installs/sway.sh
+++ src/installs/sway.sh
@@ -1,4 +1,4 @@
-sudo apt install -y sway swayidle swaylock waybar grimshot
+sudo apt install -y sway swayidle swaylock waybar grimshot blueman nm-tray brightnessctl pulseaudio-utils
 
 # For screen sharing
 sudo apt install -y xdg-desktop-portal xdg-desktop-portal-wlr
